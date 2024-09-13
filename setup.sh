87f807d89f0da9fab9507d91c796064dbc8d4643
Aaron Karlsen <aaronkarlsen@KarlsenFamily.onmicrosoft.com>
2024-09-13 12:32:47 +1000

    using nvdia beta drivers and made a really basic version of the script

--- setup.sh
+++ setup.sh
@@ -1,180 +1,184 @@
 #!/bin/bash
 
-# Check if script is run as root
-if [ "$(id -u)" -eq 0 ]; then
-    echo "This script should not be run as root. Please run it as a regular user."
-    exit 1
+# set some colors
+CNT="[\e[1;36mNOTE\e[0m]"
+COK="[\e[1;32mOK\e[0m]"
+CER="[\e[1;31mERROR\e[0m]"
+CAT="[\e[1;37mATTENTION\e[0m]"
+CWR="[\e[1;35mWARNING\e[0m]"
+CAC="[\e[1;33mACTION\e[0m]"
+INSTLOG="install.log"
+
+# clear the screen
+clear
+
+# Define ISNVIDIA variable early in the script
+if lspci | grep -i nvidia &>/dev/null; then
+    ISNVIDIA=true
+    echo -e "$CNT - NVIDIA GPU detected."
+else
+    ISNVIDIA=false
+    echo -e "$CNT - NVIDIA GPU not detected."
 fi
 
-LOG_FILE=~/setup.log
-exec > >(tee -a "$LOG_FILE") 2>&1
+# attempt to discover if this is a VM or not
+echo -e "$CNT - Checking for Physical or VM..."
+ISVM=$(hostnamectl | grep Chassis)
+echo -e "Using $ISVM"
+if [[ $ISVM == *"vm"* ]]; then
+    echo -e "$CWR - Please note that VMs are not fully supported and if you try to run this on
+    a Virtual Machine there is a high chance this will fail."
+    sleep 1
+fi
 
-echo "Starting Hyprland setup script..." $(date)
+# let the user know that we will use sudo
+echo -e "$CNT - This script will run some commands that require sudo. You will be prompted to enter your password.
+If you are worried about entering your password then you may want to review the content of the script."
+sleep 1
 
-# Function to check last command status
-check_status() {
-    if [ $? -eq 0 ]; then
-        echo "Success: $1"
+#### Check for package manager ####
+ISYAY=/sbin/yay
+if [ -f "$ISYAY" ]; then 
+    echo -e "$COK - yay was located, moving on."
+else 
+    echo -e "$CWR - Yay was NOT located.. yay is (still) required"
+    read -rep $'[\e[1;33mACTION\e[0m] - Would you like to install yay (y,n) ' INSTYAY
+    if [[ $INSTYAY == "Y" || $INSTYAY == "y" ]]; then
+        git clone https://aur.archlinux.org/yay.git &>> $INSTLOG
+        cd yay
+        makepkg -si --noconfirm &>> ../$INSTLOG
+        cd ..
     else
-        echo "Error: $1 failed"
-        exit 1
+        echo -e "$CER - Yay is (still) required for this script, now exiting"
+        exit
+    fi
+    # update the yay database
+    echo -e "$CNT - Updating the yay database..."
+    yay -Suy --noconfirm &>> $INSTLOG
+fi
+
+install_software() {
+    # First lets see if the package is there
+    if yay -Q $1 &>> /dev/null ; then
+        echo -e "$COK - $1 is already installed."
+    else
+        # no package found so installing
+        echo -e "$CNT - Now installing $1 ..."
+        yay -S --noconfirm $1 &>> $INSTLOG
+        # test to make sure package installed
+        if yay -Q $1 &>> /dev/null ; then
+            echo -e "\e[1A\e[K$COK - $1 was installed."
+        else
+            # if this is hit then a package is missing, exit to review log
+            echo -e "\e[1A\e[K$CER - $1 install had failed, please check the install.log"
+            exit
+        fi
     fi
 }
 
-# Update system
-echo "Updating system..."
-sudo pacman -Syu --noconfirm
-check_status "System update"
+if [ "$ISNVIDIA" = true ]; then
+    echo -e "$CNT - Nvidia setup stage, this may take a while..."
 
-# Install yay if not already installed
-if ! command -v yay &> /dev/null; then
-    echo "Installing yay..."
-    sudo pacman -S --needed git base-devel
-    git clone https://aur.archlinux.org/yay.git
-    cd yay
-    makepkg -si --noconfirm
-    cd ..
-    rm -rf yay
-    check_status "Yay installation"
+    for SOFTWR in linux-headers nvidia-beta nvidia-utils-beta nvidia-settings-beta lib32-nvidia-utils-beta qt5-wayland qt5ct libva libva-nvidia-driver-git
+    do
+        install_software $SOFTWR
+    done
+
+    # Modify mkinitcpio.conf
+    sudo sed -i 's/MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
+    sudo mkinitcpio --config /etc/mkinitcpio.conf --generate /boot/initramfs-custom.img
+    echo -e "options nvidia-drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia.conf &>> $INSTLOG
+
+    # Update GRUB to use the custom initramfs image
+    sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="/GRUB_CMDLINE_LINUX_DEFAULT="nvidia_drm.modeset=1 /' /etc/default/grub
+    sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+    # Set up necessary environment variables
+    echo -e "$CNT - Setting up environment variables for NVIDIA + Wayland..."
+    sudo tee -a /etc/environment << EOF
+LIBVA_DRIVER_NAME=nvidia
+XDG_SESSION_TYPE=wayland
+GBM_BACKEND=nvidia-drm
+__GLX_VENDOR_LIBRARY_NAME=nvidia
+WLR_RENDERER=vulkan
+XCURSOR_SIZE=24
+EOF
+
 fi
 
-# Install required packages
-echo "Installing required packages..."
-yay -S --needed --noconfirm hyprland kitty sddm xdg-desktop-portal-hyprland xf86-video-nouveau mesa wofi polkit-kde-agent xdg-user-dirs pipewire wireplumber
-check_status "Package installation"
+# Stage 1 - main components
+echo -e "$CNT - Stage 1 - Installing main components, this may take a while..."
+for SOFTWR in hyprland kitty # waybar jq mako swww swaylock-effects wofi wlogout xdg-desktop-portal-hyprland swappy grim slurp thunar
+do
+    install_software $SOFTWR 
+done
 
-# Enable SDDM
-echo "Enabling SDDM service..."
-sudo systemctl enable sddm.service
-check_status "SDDM service enablement"
+# Stage 2 - more tools
+# echo -e "$CNT - Stage 2 - Installing additional tools and utilities, this may take a while..."
+# for SOFTWR in polkit-gnome python-requests pamixer pavucontrol brightnessctl bluez bluez-utils blueman network-manager-applet gvfs thunar-archive-plugin file-roller btop pacman-contrib
+# do
+#     install_software $SOFTWR
+# done
+# 
+# echo -e "$CNT - Stage 3 - Installing theme and visual related tools and utilities, this may take a while..."
+# for SOFTWR in starship ttf-jetbrains-mono-nerd noto-fonts-emoji lxappearance xfce4-settings sddm qt5-svg qt5-quickcontrols2 qt5-graphicaleffects
+# do
+#     install_software $SOFTWR
+# done
 
-# Set up user groups and services
-echo "Setting up user groups and services..."
-sudo usermod -aG video,input $(whoami)
-echo "i2c-dev" | sudo tee /etc/modules-load.d/i2c-dev.conf
-systemctl --user enable --now pipewire.service pipewire-pulse.service wireplumber.service
-check_status "User groups and services setup"
+# Start the bluetooth service
+# echo -e "$CNT - Starting the Bluetooth Service..."
+# sudo systemctl enable --now bluetooth.service &>> $INSTLOG
+# sleep 2
 
-# Set up environment variables
-echo "Setting up environment variables..."
-cat << EOF >> ~/.bash_profile
-export XDG_CURRENT_DESKTOP=Hyprland
-export XDG_SESSION_TYPE=wayland
-export XDG_SESSION_DESKTOP=Hyprland
-export QT_QPA_PLATFORM=wayland
-export GDK_BACKEND=wayland
-EOF
-check_status "Environment variables setup"
+# Enable the sddm login manager service
+echo -e "$CNT - Enabling the SDDM Service..."
+# sudo systemctl enable sddm &>> $INSTLOG
+sleep 2
 
-# Create necessary directories
-echo "Creating necessary directories..."
-mkdir -p ~/.config/{hypr,kitty,wofi}
-check_status "Directory creation"
+# Clean out other portals
+# echo -e "$CNT - Cleaning out conflicting xdg portals..."
+# yay -R --noconfirm xdg-desktop-portal-gnome xdg-desktop-portal-gtk &>> $INSTLOG
 
-# Set up basic Hyprland configuration
-echo "Setting up basic Hyprland configuration..."
-cat << EOF > ~/.config/hypr/hyprland.conf
-monitor=,preferred,auto,1
+# New section for copying and linking configuration files
+echo -e "$CNT - Setting up configuration files..."
 
-exec-once = waybar & hyprpaper & firefox
+# Hyprland
+mkdir -p ~/.config/hypr
+cp "$PWD/dotfiles/hypr/"* ~/.config/hypr/
 
-input {
-    kb_layout = us
-    follow_mouse = 1
-    touchpad {
-        natural_scroll = false
-    }
-    sensitivity = 0
-}
 
-general {
-    gaps_in = 5
-    gaps_out = 20
-    border_size = 2
-    col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
-    col.inactive_border = rgba(595959aa)
-    layout = dwindle
-}
 
-decoration {
-    rounding = 10
-    blur = true
-    blur_size = 3
-    blur_passes = 1
-    blur_new_optimizations = true
-    drop_shadow = true
-    shadow_range = 4
-    shadow_render_power = 3
-    col.shadow = rgba(1a1a1aee)
-}
+ln -sf ~/.config/HyprV/kitty/kitty.conf ~/.config/kitty/kitty.conf
+# ln -sf ~/.config/HyprV/mako/conf/config-dark ~/.config/mako/config
+# ln -sf ~/.config/HyprV/swaylock/config ~/.config/swaylock/config
+# ln -sf ~/.config/HyprV/waybar/conf/v3-config.jsonc ~/.config/waybar/config.jsonc
+# ln -sf ~/.config/HyprV/waybar/style/v3-style-dark.css ~/.config/waybar/style.css
+# ln -sf ~/.config/HyprV/wlogout/layout ~/.config/wlogout/layout
+# ln -sf ~/.config/HyprV/wofi/config ~/.config/wofi/config
+# ln -sf ~/.config/HyprV/wofi/style/v3-style-dark.css ~/.config/wofi/style.css
 
-animations {
-    enabled = true
-    bezier = myBezier, 0.05, 0.9, 0.1, 1.05
-    animation = windows, 1, 7, myBezier
-    animation = windowsOut, 1, 7, default, popin 80%
-    animation = border, 1, 10, default
-    animation = borderangle, 1, 8, default
-    animation = fade, 1, 7, default
-    animation = workspaces, 1, 6, default
-}
+# Make the autostart script executable
+chmod +x ~/.config/hypr/autostart.sh
 
-dwindle {
-    pseudotile = true
-    preserve_split = true
-}
+# Check if the operations were successful
+if [ $? -eq 0 ]; then
+    echo -e "$COK - Configuration files have been set up successfully."
+else
+    echo -e "$CER - There was an error setting up the configuration files. Please check the paths and try again."
+fi
 
-master {
-    new_is_master = true
-}
+### Script is done ###
+echo -e "$CNT - Script had completed!"
+if [[ "$ISNVIDIA" == true ]]; then 
+    echo -e "$CAT - Since we attempted to setup Nvidia the script will now end and you should reboot.
+    type 'reboot' at the prompt and hit Enter when ready."
+    exit
+fi
 
-gestures {
-    workspace_swipe = false
-}
-
-bind = SUPER, Return, exec, kitty
-bind = SUPER, Q, killactive,
-bind = SUPER, M, exit,
-bind = SUPER, E, exec, dolphin
-bind = SUPER, V, togglefloating,
-bind = SUPER, R, exec, wofi --show drun
-bind = SUPER, P, pseudo,
-bind = SUPER, J, togglesplit,
-
-bind = SUPER, left, movefocus, l
-bind = SUPER, right, movefocus, r
-bind = SUPER, up, movefocus, u
-bind = SUPER, down, movefocus, d
-
-bind = SUPER, 1, workspace, 1
-bind = SUPER, 2, workspace, 2
-bind = SUPER, 3, workspace, 3
-bind = SUPER, 4, workspace, 4
-bind = SUPER, 5, workspace, 5
-bind = SUPER, 6, workspace, 6
-bind = SUPER, 7, workspace, 7
-bind = SUPER, 8, workspace, 8
-bind = SUPER, 9, workspace, 9
-bind = SUPER, 0, workspace, 10
-
-bind = SUPER SHIFT, 1, movetoworkspace, 1
-bind = SUPER SHIFT, 2, movetoworkspace, 2
-bind = SUPER SHIFT, 3, movetoworkspace, 3
-bind = SUPER SHIFT, 4, movetoworkspace, 4
-bind = SUPER SHIFT, 5, movetoworkspace, 5
-bind = SUPER SHIFT, 6, movetoworkspace, 6
-bind = SUPER SHIFT, 7, movetoworkspace, 7
-bind = SUPER SHIFT, 8, movetoworkspace, 8
-bind = SUPER SHIFT, 9, movetoworkspace, 9
-bind = SUPER SHIFT, 0, movetoworkspace, 10
-
-bind = SUPER, mouse_down, workspace, e+1
-bind = SUPER, mouse_up, workspace, e-1
-
-bindm = SUPER, mouse:272, movewindow
-bindm = SUPER, mouse:273, resizewindow
-EOF
-check_status "Hyprland configuration setup"
-
-echo "Setup completed successfully. Please reboot your system to start using your new Hyprland setup."
-echo "After reboot, select Hyprland as your session at the login screen."
+# read -rep $'[\e[1;33mACTION\e[0m] - Would you like to start Hyprland now? (y,n) ' HYP
+# if [[ $HYP == "Y" || $HYP == "y" ]]; then
+#     exec sudo systemctl start sddm &>> $INSTLOG
+# else
+#     exit
+# fi
