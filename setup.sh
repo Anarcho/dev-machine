44c10cccdd13e1a6dd54bd3e84e8c07e60278eb7
Aaron Karlsen <aaronkarlsen@KarlsenFamily.onmicrosoft.com>
2024-09-14 03:28:53 +1000

    simplified and adjusted some logic

--- setup.sh
+++ setup.sh
@@ -1,51 +1,64 @@
 #!/bin/bash
 
-# set some colors
+# Set some colors for logging
 CNT="[\e[1;36mNOTE\e[0m]"
 COK="[\e[1;32mOK\e[0m]"
 CER="[\e[1;31mERROR\e[0m]"
-CAT="[\e[1;37mATTENTION\e[0m]"
 CWR="[\e[1;35mWARNING\e[0m]"
-CAC="[\e[1;33mACTION\e[0m]"
-INSTLOG="install.log"
 
-# Set the location for your dotfiles
-DOTFILES_DIR="$HOME/.dotfiles"
+# Default paths and URLs (as a fallback)
+DEFAULT_DOTFILES_DIR="$HOME/.dotfiles"
+DEFAULT_DOTFILES_REPO="https://github.com/anarcho/dotfiles.git"
 
-# URL of your dotfiles repository
-DOTFILES_REPO="https://github.com/anarcho/dotfiles.git"
+# Function to check and fix environment variables
+check_env_var() {
+    local var_name=$1
+    local fallback_value=$2
+    local var_value=${!var_name}
 
-# List of configurations to stow
-configs=("hypr" "kitty" "waybar")
+    if [ -z "$var_value" ]; then
+        echo -e "$CWR - $var_name is not set. Attempting to set it to default value."
+        export "$var_name"="$fallback_value"
+        echo "export $var_name=\"$fallback_value\"" >> ~/.bashrc
+        echo -e "$COK - $var_name set to $fallback_value."
+    else
+        echo -e "$CNT - $var_name is set to $var_value. Verifying..."
+        if [[ "$var_name" == "DOTFILES_DIR" ]]; then
+            if [ -d "$var_value" ]; then
+                echo -e "$COK - $var_name path is valid."
+            else
+                echo -e "$CWR - $var_name directory does not exist. Creating it."
+                mkdir -p "$var_value"
+                if [ $? -eq 0 ]; then
+                    echo -e "$COK - $var_name directory created."
+                else
+                    echo -e "$CER - Failed to create $var_name directory. Falling back to default."
+                    export "$var_name"="$fallback_value"
+                fi
+            fi
+        elif [[ "$var_name" == "DOTFILES_REPO" ]]; then
+            if curl --output /dev/null --silent --head --fail "$var_value"; then
+                echo -e "$COK - $var_name URL is reachable."
+            else
+                echo -e "$CER - $var_name URL is not reachable. Falling back to default."
+                export "$var_name"="$fallback_value"
+            fi
+        fi
+    fi
+}
 
-# # Function to check if variable is already in .bashrc
-# add_to_bashrc() {
-#     VAR_NAME=$1
-#     VAR_VALUE=$2
-# 
-#     # Check if the variable is already set in .bashrc
-#     if grep -q "^export $VAR_NAME=" ~/.bashrc; then
-#         echo -e "$CWR - $VAR_NAME is already set in ~/.bashrc. Updating its value."
-#         sed -i "s|^export $VAR_NAME=.*|export $VAR_NAME=\"$VAR_VALUE\"|" ~/.bashrc
-#     else
-#         echo -e "$CNT - Adding $VAR_NAME to ~/.bashrc."
-#         echo "export $VAR_NAME=\"$VAR_VALUE\"" >> ~/.bashrc
-#     fi
-# }
-# 
-# # Add DOTFILES_DIR and DOTFILES_REPO to ~/.bashrc
-# add_to_bashrc "DOTFILES_DIR" "$DOTFILES_DIR"
-# add_to_bashrc "DOTFILES_REPO" "$DOTFILES_REPO"
-# 
-# source ~/.bashrc
+# Check and fix environment variables for DOTFILES_DIR and DOTFILES_REPO
+check_env_var "DOTFILES_DIR" "$DEFAULT_DOTFILES_DIR"
+check_env_var "DOTFILES_REPO" "$DEFAULT_DOTFILES_REPO"
 
-# Let the user know they need to source .bashrc or restart terminal
-echo -e "$COK - Added variables to ~/.bashrc. Please run 'source ~/.bashrc' or restart your terminal to apply the changes."
+# Source .bashrc to make sure environment variables are loaded
+source ~/.bashrc
 
-# clear the screen
+# Clear the screen
 clear
 
-# Define ISNVIDIA variable early in the script
+# NVIDIA check and VM check
+echo -e "$CNT - Checking for NVIDIA GPU..."
 if lspci | grep -i nvidia &>/dev/null; then
     ISNVIDIA=true
     echo -e "$CNT - NVIDIA GPU detected."
@@ -54,133 +67,79 @@ else
     echo -e "$CNT - NVIDIA GPU not detected."
 fi
 
-# attempt to discover if this is a VM or not
-echo -e "$CNT - Checking for Physical or VM..."
-ISVM=$(hostnamectl | grep Chassis)
-echo -e "Using $ISVM"
-if [[ $ISVM == *"vm"* ]]; then
+echo -e "$CNT - Checking if system is a VM..."
+ISVM=$(hostnamectl | grep Chassis | grep -o 'vm')
+if [[ $ISVM == "vm" ]]; then
+    echo -e "$CWR - Running in a VM."
     ISVM=true
-    echo -e "$CWR - Please note that VMs are not fully supported and if you try to run this on
-    a Virtual Machine there is a high chance this will fail."
-    sleep 1
 else
     ISVM=false
 fi
 
-# let the user know that we will use sudo
-echo -e "$CNT - This script will run some commands that require sudo. You will be prompted to enter your password.
-If you are worried about entering your password then you may want to review the content of the script."
-sleep 1
-
-#### Check for package manager ####
+# Ensure yay is installed
 ISYAY=/sbin/yay
-if [ -f "$ISYAY" ]; then 
-    echo -e "$COK - yay was located, moving on."
-else 
-    echo -e "$CWR - Yay was NOT located.. yay is (still) required"
-    read -rep $'[\e[1;33mACTION\e[0m] - Would you like to install yay (y,n) ' INSTYAY
-    if [[ $INSTYAY == "Y" || $INSTYAY == "y" ]]; then
-        git clone https://aur.archlinux.org/yay.git &>> $INSTLOG
-        cd yay
-        makepkg -si --noconfirm &>> ../$INSTLOG
-        cd ..
+if [ ! -f "$ISYAY" ]; then
+    echo -e "$CWR - Yay is not installed."
+    read -rp $'[\e[1;33mACTION\e[0m] - Would you like to install yay? (y/n) ' INSTYAY
+    if [[ $INSTYAY =~ [Yy] ]]; then
+        git clone https://aur.archlinux.org/yay.git &>> install.log
+        cd yay && makepkg -si --noconfirm &>> ../install.log && cd ..
+        echo -e "$COK - Yay installed."
     else
-        echo -e "$CER - Yay is (still) required for this script, now exiting"
-        exit
+        echo -e "$CER - Yay is required. Exiting."
+        exit 1
     fi
-    # update the yay database
-    echo -e "$CNT - Updating the yay database..."
-    yay -Suy --noconfirm &>> $INSTLOG
 fi
 
+# Function to install software
 install_software() {
-    # First lets see if the package is there
-    if yay -Q $1 &>> /dev/null ; then
+    if yay -Q $1 &>/dev/null; then
         echo -e "$COK - $1 is already installed."
     else
-        # no package found so installing
-        echo -e "$CNT - Now installing $1 ..."
-        yes | yay -S --noconfirm $1 &>> $INSTLOG
-        # test to make sure package installed
-        if yay -Q $1 &>> /dev/null ; then
-            echo -e "\e[1A\e[K$COK - $1 was installed."
+        echo -e "$CNT - Installing $1 ..."
+        yes | yay -S --noconfirm $1 &>> install.log
+        if yay -Q $1 &>/dev/null; then
+            echo -e "$COK - $1 was installed successfully."
         else
-            # if this is hit then a package is missing, exit to review log
-            echo -e "\e[1A\e[K$CER - $1 install had failed, please check the install.log"
-            exit
+            echo -e "$CER - Failed to install $1. Check install.log."
+            exit 1
         fi
     fi
 }
 
-# Run NVIDIA setup only if not in a VM and NVIDIA GPU is detected
-if [ "$ISNVIDIA" = true ] && [ "$ISVM" = false ]; then
-    echo -e "$CNT - Nvidia setup stage, this may take a while..."
-
-    # First, remove potentially conflicting NVIDIA packages
-    echo -e "$CNT - Removing potentially conflicting NVIDIA packages..."
-    yes | sudo pacman -Rd nvidia 2>/dev/null
-    yes | yay -Rd nvidia 2>/dev/null
-
-    yes | sudo pacman -Rd nvidia-utils 2>/dev/null
-    yes | yay -Rd nvidia-utils 2>/dev/null
-
-    yes | sudo pacman -Rd nvidia-settings 2>/dev/null
-    yes | yay -Rd nvidia-settings 2>/dev/null
-
-    # Install NVIDIA packages
-    for SOFTWR in linux-headers nvidia-beta nvidia-utils-beta nvidia-settings-beta lib32-nvidia-utils-beta qt5-wayland qt5ct libva libva-nvidia-driver-git
-    do
-        echo -e "$CNT - Installing $SOFTWR..."
-        yes | yay -S --noconfirm --answerdiff None --answerclean None --mflags "--noconfirm" $SOFTWR
-        if [ $? -ne 0 ]; then
-            echo -e "$CER - Failed to install $SOFTWR. Please check the logs and try again."
-            exit 1
-        fi
+# NVIDIA setup (if not a VM and NVIDIA detected)
+if [[ "$ISNVIDIA" == true && "$ISVM" == false ]]; then
+    echo -e "$CNT - Setting up NVIDIA..."
+    for pkg in linux-headers nvidia nvidia-utils nvidia-settings mkinitcpio; do
+        install_software $pkg
     done
 
-    # Modify mkinitcpio.conf
+    # Update mkinitcpio.conf
     sudo sed -i 's/MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
-    sudo mkinitcpio --config /etc/mkinitcpio.conf --generate /boot/initramfs-custom.img
-    echo -e "options nvidia-drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia.conf &>> $INSTLOG
-
-    # Update GRUB to use the custom initramfs image
-    sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="/GRUB_CMDLINE_LINUX_DEFAULT="nvidia_drm.modeset=1 /' /etc/default/grub
+    sudo mkinitcpio -P
+    echo -e "options nvidia-drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia.conf &>> install.log
     sudo grub-mkconfig -o /boot/grub/grub.cfg
 
-    # Set up necessary environment variables
-    echo -e "$CNT - Setting up environment variables for NVIDIA + Wayland..."
+    # Set environment variables for NVIDIA and Wayland
     sudo tee -a /etc/environment << EOF
 LIBVA_DRIVER_NAME=nvidia
 XDG_SESSION_TYPE=wayland
 GBM_BACKEND=nvidia-drm
 __GLX_VENDOR_LIBRARY_NAME=nvidia
 WLR_RENDERER=vulkan
-XCURSOR_SIZE=24
 EOF
-
 else
-    if [ "$ISVM" = true ]; then
-        echo -e "$CNT - Running in a VM, skipping NVIDIA setup."
-    else
-        echo -e "$CNT - No NVIDIA GPU detected, skipping NVIDIA setup."
-    fi
+    echo -e "$CNT - Skipping NVIDIA setup."
 fi
-
-# Stage 1 - main components
-echo -e "$CNT - Stage 1 - Installing main components, this may take a while..."
-for SOFTWR in hyprland kitty neovim stow waybar #jq mako swww swaylock-effects wofi wlogout xdg-desktop-portal-hyprland swappy grim slurp thunar
-do
-    install_software $SOFTWR 
-done
 
 # Clone or update dotfiles
 echo -e "$CNT - Setting up dotfiles..."
-if [ -d "~/.dotfiles" ]; then
-    echo -e "$CNT - Dotfiles directory already exists. Updating..."
-    cd "~/.dotfiles" && git pull
+if [ -d "$DOTFILES_DIR" ]; then
+    echo -e "$CNT - Dotfiles directory exists. Pulling updates..."
+    cd "$DOTFILES_DIR" && git pull
 else
     echo -e "$CNT - Cloning dotfiles repository..."
-    git clone "https://github.com/anarcho/dotfiles.git" "~/.dotfiles"
+    git clone "$DOTFILES_REPO" "$DOTFILES_DIR"
 fi
 
 if [ $? -eq 0 ]; then
@@ -190,37 +149,29 @@ else
     exit 1
 fi
 
-# clear symlinks
+# List of configs to stow
+configs=("hypr" "kitty" "waybar")
 
-# Hypr
-echo -e "$CNT - Clearing Hyprland symlinks..."
-find "$HOME/.config/hypr" -maxdepth 1 -type l -delete
+# Clearing old symlinks
+clear_symlinks() {
+    local dir=$1
+    if [ -d "$dir" ]; then
+        echo -e "$CNT - Clearing symlinks in $dir..."
+        find "$dir" -maxdepth 1 -type l -delete
+    else
+        echo -e "$CWR - Directory $dir not found. Skipping."
+    fi
+}
+clear_symlinks "$HOME/.config/hypr"
+clear_symlinks "$HOME/.config/kitty"
+clear_symlinks "$HOME/.config/waybar"
 
-# kitty
-echo -e "$CNT - Clearing kitty symlinks..."
-find "$HOME/.config/kitty" -maxdepth 1 -type l -delete
-
-# waybar
-echo -e "$CNT - Clearing waybar symlinks..."
-find "$HOME/.config/waybar" -maxdepth 1 -type l -delete
-
-# Use Stow to manage dotfiles
+# Use stow to symlink the dotfiles
 echo -e "$CNT - Using stow to symlink dotfiles..."
+cd "$DOTFILES_DIR" || { echo -e "$CER - Failed to change to $DOTFILES_DIR. Exiting."; exit 1; }
 
-cd "$DOTFILES_DIR" || { echo -e "$CER - Failed to change directory to $DOTFILES_DIR"; exit 1; }
-
-# Iterate over the list of configs and stow them
 for config in "${configs[@]}"; do
-    echo -e "$CNT - Stowing $config..."
     stow -R "$config" && echo -e "$COK - Successfully stowed $config." || echo -e "$CER - Failed to stow $config."
 done
 
 echo -e "$COK - Configuration files have been symlinked successfully."
-
-### Script is done ###
-echo -e "$CNT - Script has completed!"
-if [[ "$ISNVIDIA" == true ]]; then 
-    echo -e "$CAT - Since we attempted to set up Nvidia, the script will now end and you should reboot.
-    Type 'reboot' at the prompt and hit Enter when ready."
-    exit
-fi
--- update_config.sh
+++ update_config.sh
@@ -4,48 +4,71 @@
 CNT="[\e[1;36mNOTE\e[0m]"
 COK="[\e[1;32mOK\e[0m]"
 CER="[\e[1;31mERROR\e[0m]"
-CAT="[\e[1;37mATTENTION\e[0m]"
 CWR="[\e[1;35mWARNING\e[0m]"
 
-# Set the location for your dotfiles
-DOTFILES_DIR="$HOME/.dotfiles"
+# Default paths and URLs (as a fallback)
+DEFAULT_DOTFILES_DIR="$HOME/.dotfiles"
+DEFAULT_DOTFILES_REPO="https://github.com/anarcho/dotfiles.git"
 
-# URL of your dotfiles repository
-DOTFILES_REPO="https://github.com/anarcho/dotfiles.git"
+# Function to check and fix environment variables
+check_env_var() {
+    local var_name=$1
+    local fallback_value=$2
+    local var_value=${!var_name}
 
-# List of configurations to stow
-configs=("hypr" "kitty" "waybar")
-
-# Function to check if variable is already in .bashrc
-add_to_bashrc() {
-    VAR_NAME=$1
-    VAR_VALUE=$2
-
-    # Check if the variable is already set in .bashrc
-    if grep -q "^export $VAR_NAME=" ~/.bashrc; then
-        echo -e "$CWR - $VAR_NAME is already set in ~/.bashrc. Updating its value."
-        sed -i "s|^export $VAR_NAME=.*|export $VAR_NAME=\"$VAR_VALUE\"|" ~/.bashrc
+    if [ -z "$var_value" ]; then
+        echo -e "$CWR - $var_name is not set. Attempting to set it to default value."
+        export "$var_name"="$fallback_value"
+        echo "export $var_name=\"$fallback_value\"" >> ~/.bashrc
+        echo -e "$COK - $var_name set to $fallback_value."
     else
-        echo -e "$CNT - Adding $VAR_NAME to ~/.bashrc."
-        echo "export $VAR_NAME=\"$VAR_VALUE\"" >> ~/.bashrc
+        echo -e "$CNT - $var_name is set to $var_value. Verifying..."
+        # Verify if directory exists for DOTFILES_DIR, or if URL is reachable for DOTFILES_REPO
+        if [[ "$var_name" == "DOTFILES_DIR" ]]; then
+            if [ -d "$var_value" ]; then
+                echo -e "$COK - $var_name path is valid."
+            else
+                echo -e "$CWR - $var_name directory does not exist. Creating it."
+                mkdir -p "$var_value"
+                if [ $? -eq 0 ]; then
+                    echo -e "$COK - $var_name directory created."
+                else
+                    echo -e "$CER - Failed to create $var_name directory. Falling back to default."
+                    export "$var_name"="$fallback_value"
+                fi
+            fi
+        elif [[ "$var_name" == "DOTFILES_REPO" ]]; then
+            if curl --output /dev/null --silent --head --fail "$var_value"; then
+                echo -e "$COK - $var_name URL is reachable."
+            else
+                echo -e "$CER - $var_name URL is not reachable. Falling back to default."
+                export "$var_name"="$fallback_value"
+            fi
+        fi
     fi
 }
 
-# Add DOTFILES_DIR and DOTFILES_REPO to ~/.bashrc
-add_to_bashrc "DOTFILES_DIR" "$DOTFILES_DIR"
-add_to_bashrc "DOTFILES_REPO" "$DOTFILES_REPO"
+# Check and fix environment variables for DOTFILES_DIR and DOTFILES_REPO
+check_env_var "DOTFILES_DIR" "$DEFAULT_DOTFILES_DIR"
+check_env_var "DOTFILES_REPO" "$DEFAULT_DOTFILES_REPO"
+
+# Source .bashrc to make sure environment variables are loaded
+source ~/.bashrc
 
 # Let the user know they need to source .bashrc or restart terminal
-echo -e "$COK - Added variables to ~/.bashrc. Please run 'source ~/.bashrc' or restart your terminal to apply the changes."
+echo -e "$COK - Added/Updated environment variables to ~/.bashrc. Please run 'source ~/.bashrc' or restart your terminal to apply the changes."
 
-## Clone or update dotfiles
+# Clear the screen
+clear
+
+# Clone or update dotfiles
 echo -e "$CNT - Setting up dotfiles..."
-if [ -d "~/.dotfiles" ]; then
+if [ -d "$DOTFILES_DIR" ]; then
     echo -e "$CNT - Dotfiles directory already exists. Updating..."
-    cd "~/.dotfiles" && git pull
+    cd "$DOTFILES_DIR" && git pull
 else
     echo -e "$CNT - Cloning dotfiles repository..."
-    git clone "https://github.com/anarcho/dotfiles.git" "~/.dotfiles"
+    git clone "$DOTFILES_REPO" "$DOTFILES_DIR"
 fi
 
 if [ $? -eq 0 ]; then
@@ -55,46 +78,131 @@ else
     exit 1
 fi
 
-# clear symlinks
+# List of configurations to stow
+configs=("hypr" "kitty" "waybar")
 
-# Hypr
-echo -e "$CNT - Clearing Hyprland symlinks..."
-find "$HOME/.config/hypr" -maxdepth 1 -type l -delete
+# Let the user know they need to source .bashrc or restart terminal
+echo -e "$COK - Added variables to ~/.bashrc. Please run 'source ~/.bashrc' or restart your terminal to apply the changes."
 
-# kitty
-echo -e "$CNT - Clearing kitty symlinks..."
-find "$HOME/.config/kitty" -maxdepth 1 -type l -delete
+# clear the screen
+clear
 
-# waybar
-echo -e "$CNT - Clearing waybar symlinks..."
-find "$HOME/.config/waybar" -maxdepth 1 -type l -delete
+# Function to check if a directory exists and clear symlinks
+clear_symlinks() {
+    local dir=$1
+    if [ -d "$dir" ]; then
+        echo -e "$CNT - Clearing symlinks in $dir..."
+        find "$dir" -maxdepth 1 -type l -delete
+    else
+        echo -e "$CWR - Directory $dir not found. Skipping symlink clearing."
+    fi
+}
 
+# Check for NVIDIA GPU
+if lspci | grep -i nvidia &>/dev/null; then
+    ISNVIDIA=true
+    echo -e "$CNT - NVIDIA GPU detected."
+else
+    ISNVIDIA=false
+    echo -e "$CNT - NVIDIA GPU not detected."
+fi
 
-# Use Stow to manage dotfiles
+# Check if the machine is a VM
+echo -e "$CNT - Checking for Physical or VM..."
+ISVM=$(hostnamectl | grep Chassis)
+if [[ $ISVM == *"vm"* ]]; then
+    ISVM=true
+    echo -e "$CWR - VM detected. Some operations may not be supported."
+else
+    ISVM=false
+fi
+
+# Ensure sudo is needed
+echo -e "$CNT - This script will require sudo permissions."
+sleep 1
+
+# Install software function
+install_software() {
+    if yay -Q $1 &>/dev/null; then
+        echo -e "$COK - $1 is already installed."
+    else
+        echo -e "$CNT - Installing $1 ..."
+        yes | yay -S --noconfirm $1 &>> $INSTLOG
+        if yay -Q $1 &>/dev/null; then
+            echo -e "$COK - $1 was installed."
+        else
+            echo -e "$CER - Failed to install $1. Please check install.log"
+            exit 1
+        fi
+    fi
+}
+
+# NVIDIA setup (if not in VM)
+if [ "$ISNVIDIA" = true ] && [ "$ISVM" = false ]; then
+    echo -e "$CNT - Nvidia setup..."
+    for pkg in linux-headers nvidia nvidia-utils nvidia-settings lib32-nvidia-utils qt5-wayland qt5ct; do
+        install_software $pkg
+    done
+
+    # Nvidia configuration
+    sudo sed -i 's/MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
+    sudo mkinitcpio --generate /boot/initramfs-linux.img
+    sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="/GRUB_CMDLINE_LINUX_DEFAULT="nvidia-drm.modeset=1 /' /etc/default/grub
+    sudo grub-mkconfig -o /boot/grub/grub.cfg
+    sudo tee -a /etc/environment <<EOF
+LIBVA_DRIVER_NAME=nvidia
+XDG_SESSION_TYPE=wayland
+GBM_BACKEND=nvidia-drm
+EOF
+else
+    if [ "$ISVM" = true ]; then
+        echo -e "$CNT - VM detected. Skipping NVIDIA setup."
+    else
+        echo -e "$CNT - No NVIDIA GPU detected. Skipping NVIDIA setup."
+    fi
+fi
+
+# Install main components
+echo -e "$CNT - Installing main components..."
+for pkg in hyprland kitty neovim stow waybar; do
+    install_software $pkg
+done
+
+# Clone or update dotfiles
+echo -e "$CNT - Setting up dotfiles..."
+if [ -d "$DOTFILES_DIR" ]; then
+    echo -e "$CNT - Dotfiles directory already exists. Updating..."
+    cd "$DOTFILES_DIR" && git pull
+else
+    echo -e "$CNT - Cloning dotfiles repository..."
+    git clone "$DOTFILES_REPO" "$DOTFILES_DIR"
+fi
+
+if [ $? -eq 0 ]; then
+    echo -e "$COK - Dotfiles setup successful."
+else
+    echo -e "$CER - Failed to setup dotfiles. Exiting."
+    exit 1
+fi
+
+# Clear old symlinks
+clear_symlinks "$HOME/.config/hypr"
+clear_symlinks "$HOME/.config/kitty"
+clear_symlinks "$HOME/.config/waybar"
+
+# Use stow to manage dotfiles
 echo -e "$CNT - Using stow to refresh dotfiles..."
+cd "$DOTFILES_DIR" || { echo -e "$CER - Failed to change directory to $DOTFILES_DIR"; exit 1; }
 
-cd "~/.dotfiles" || { echo -e "$CER - Failed to change directory to ~/.dotfiles"; exit 1; }
-
-# Iterate over the list of configs and stow them
 for config in "${configs[@]}"; do
     echo -e "$CNT - Stowing $config..."
     stow -R "$config" && echo -e "$COK - Successfully stowed $config." || echo -e "$CER - Failed to stow $config."
 done
 
-# Make the Hyprland autostart script executable (if it exists)
-if [ -f "$HOME/.config/hypr/autostart.sh" ]; then
-    chmod +x "$HOME/.config/hypr/autostart.sh"
-    echo -e "$COK Made Hyprland autostart script executable."
-fi
+# Make scripts executable
+chmod +x "$HOME/.config/waybar/scripts/powermenu.sh" 2>/dev/null
 
-# Make way bar scripts executable
-
-if [ -f "$HOME/.config/waybar/scripts/powermenu.sh" ]; then
-    chmod +x "$HOME/.config/waybar/scripts/powermenu.sh"
-    echo -e "$COK Made waybar battery script executable."
-fi
-
-echo -e "$CNT Configuration update complete!"
+echo -e "$CNT - Configuration update complete!"
 
 # Prompt to restart Hyprland
 read -rp $'[\e[1;33mACTION\e[0m] - Would you like to restart Hyprland to apply changes? (y/n) ' restart
